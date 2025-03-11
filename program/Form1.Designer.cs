ae4fa51c86c5cd94279e32be315298b8f206ef51
Kris The Red Panda <117139771+kristheredpanda@users.noreply.github.com>
2025-03-11 18:17:01 +1100

    Add files via upload

---
+++ program/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Wallpaper_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wallpaper Changer V1.0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(20, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "current wallpaper";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listBox1.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(20, 351);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(694, 166);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(24, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "choose wallpaper";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(20, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(694, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Set as wallpaper";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(374, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(201, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "selected wallpaper";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.pictureBox2.Location = new System.Drawing.Point(374, 110);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(340, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(611, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 42);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "About";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(503, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 42);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "GitHub";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(726, 582);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wallpaper Changer V1.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ program/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Wallpaper_Changer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class wallpaper
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, string vParam, UInt32 winIni);
+
+            public static void change(string path)
+            {
+                SystemParametersInfo(0x14, 0, path, 0x01 | 0x02);
+            }
+        }
+
+        public void loadfwscriptstage1()
+        {
+            if (!Directory.Exists(Application.StartupPath + "\\scripts"))
+            {
+                Directory.CreateDirectory(Application.StartupPath + "\\scripts");
+                loadfwscriptstage2();
+            }
+            else if (Directory.Exists(Application.StartupPath + "\\scripts"))
+            {
+                loadfwscriptstage2();
+            }
+        }
+
+        public void loadfwscriptstage2()
+        {
+            string fwscriptlocation = Application.StartupPath + "\\scripts\\findwallpaper.vbs";
+
+            File.Create(fwscriptlocation).Close();
+
+            StreamWriter sw = new StreamWriter(fwscriptlocation);
+            sw.WriteLine("Const HKCU = &H80000001 'HKEY_CURRENT_USER");
+            sw.WriteLine("");
+            sw.WriteLine("sComputer = \".\"");
+            sw.WriteLine("");
+            sw.WriteLine("Set oReg=GetObject(\"winmgmts:{impersonationLevel=impersonate}!\\\\\" _");
+            sw.WriteLine("            & sComputer & \"\\root\\default:StdRegProv\")");
+            sw.WriteLine("");
+            sw.WriteLine("sKeyPath = \"Control Panel\\Desktop\\\"");
+            sw.WriteLine("sValueName = \"TranscodedImageCache\"");
+            sw.WriteLine("oReg.GetBinaryValue HKCU, sKeyPath, sValueName, sValue");
+            sw.WriteLine("");
+            sw.WriteLine("");
+            sw.WriteLine("sContents = \"\"");
+            sw.WriteLine("");
+            sw.WriteLine("For i = 24 To UBound(sValue)");
+            sw.WriteLine("  vByte = sValue(i)");
+            sw.WriteLine("  If vByte <> 0 And vByte <> \"\" Then");
+            sw.WriteLine("    sContents = sContents & Chr(vByte)");
+            sw.WriteLine("  End If");
+            sw.WriteLine("Next");
+            sw.WriteLine("");
+            sw.WriteLine("Set fso = CreateObject(\"Scripting.FileSystemObject\")");
+            sw.WriteLine("Set fl = fso.OpenTextFile(\"" + Application.StartupPath + "\\currentwallpaperoutput.txt\", 2, True)");
+            sw.WriteLine("fl.Write(sContents)");
+            sw.WriteLine("fl.Close : Set fl = Nothing");
+            sw.WriteLine("Set fso = Nothing");
+
+            sw.Close();
+
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\scripts\\findwallpaper.vbs");
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            loadfwscriptstage3();
+            timer1.Stop();
+        }
+
+        public void loadfwscriptstage3()
+        {
+            string cwolocation = Application.StartupPath + "\\currentwallpaperoutput.txt";
+
+            StreamReader sr = new StreamReader(cwolocation);
+            string wallpaperdirectory = sr.ReadLine();
+
+            sr.Close();
+
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+
+            pictureBox1.Image = Image.FromFile(wallpaperdirectory);
+        }
+
+        private void PopulateListBox(ListBox lsb, string Folder, string FileType)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles(FileType);
+            foreach (FileInfo file in Files)
+            {
+                lsb.Items.Add(file.Name);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadfwscriptstage1();
+            PopulateListBox(listBox1, Application.StartupPath + "\\wallpapers", "*.jpg");
+            PopulateListBox(listBox1, Application.StartupPath + "\\wallpapers", "*.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            PopulateListBox(listBox1, Application.StartupPath + "\\wallpapers", "*.jpg");
+            PopulateListBox(listBox1, Application.StartupPath + "\\wallpapers", "*.png");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string wallpapersfolder = Application.StartupPath + "\\wallpapers";
+
+            if (listBox1.SelectedIndex == 0)
+            {
+                wallpaper.change(wallpapersfolder + "\\wallpaper1.jpg");
+                pictureBox1.Image = Image.FromFile(wallpapersfolder + "\\wallpaper1.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 1)
+            {
+                wallpaper.change(wallpapersfolder + "\\wallpaper2.jpg");
+                pictureBox1.Image = Image.FromFile(wallpapersfolder + "\\wallpaper2.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 2)
+            {
+                wallpaper.change(wallpapersfolder + "\\wallpaper3.jpg");
+                pictureBox1.Image = Image.FromFile(wallpapersfolder + "\\wallpaper3.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 3)
+            {
+                wallpaper.change(wallpapersfolder + "\\wallpaper4.jpg");
+                pictureBox1.Image = Image.FromFile(wallpapersfolder + "\\wallpaper4.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 4)
+            {
+                wallpaper.change(wallpapersfolder + "\\wallpaper5.jpg");
+                pictureBox1.Image = Image.FromFile(wallpapersfolder + "\\wallpaper5.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 5)
+            {
+                wallpaper.change(wallpapersfolder + "\\wallpaper6.png");
+                pictureBox1.Image = Image.FromFile(wallpapersfolder + "\\wallpaper6.png");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string wallpapersfolder = Application.StartupPath + "\\wallpapers";
+
+            if (listBox1.SelectedIndex == 0)
+            {
+                pictureBox2.Image = Image.FromFile(wallpapersfolder + "\\wallpaper1.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 1)
+            {
+                pictureBox2.Image = Image.FromFile(wallpapersfolder + "\\wallpaper2.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 2)
+            {
+                pictureBox2.Image = Image.FromFile(wallpapersfolder + "\\wallpaper3.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 3)
+            {
+                pictureBox2.Image = Image.FromFile(wallpapersfolder + "\\wallpaper4.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 4)
+            {
+                pictureBox2.Image = Image.FromFile(wallpapersfolder + "\\wallpaper5.jpg");
+            }
+
+            if (listBox1.SelectedIndex == 5)
+            {
+                pictureBox2.Image = Image.FromFile(wallpapersfolder + "\\wallpaper6.png");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/kristheredpanda/wallpaper-changer");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+    }
+}
